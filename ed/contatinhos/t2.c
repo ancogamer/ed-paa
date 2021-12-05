d0aaacade17e0717dfd31046ae4ce60280f024c8
João Vitor Astori Saletit <ancogamer@gmail.com>
2021-12-05 12:44:29 -0300

    fix somestuff

--- ed/contatinhos/t2.c
+++ ed/contatinhos/t2.c
@@ -8,84 +8,220 @@
 typedef struct C {
     char n[10];
     int nu;  
-    struct C* dad;
+    int peso;
     struct C* dir;
     struct C* esq;
       
 } C;
 
+int max(int a, int b){
+    return (a > b)? a : b;
+}
 
-void iC(C** t, int nu,char n[10],C* dad){
-    if(*t == NULL)
+int getPeso(struct C *c){
+    if (c == NULL)
+        return 0;
+    return c->peso;
+}
+
+
+int diferencapeso(struct C *c){
+    if (c == NULL)
+        return 0;
+
+    int esqR =0;
+    int dirR =0;    
+    if (c->esq != NULL)
+        esqR=c->esq->peso;
+    if (c->dir != NULL)
+        dirR=c->dir->peso;    
+
+    return esqR-dirR;
+}
+
+
+C * minValorNo(struct C* c){
+    struct C* atual = c;
+    for(;;){
+        if (atual->esq == NULL){
+            break;
+        }
+        atual = atual->esq;
+    }
+    return atual;
+    
+}
+
+C* giraDireita(struct C *c){
+    struct C *x = c->esq;
+    struct C *Coringa = x->dir;
+    x->dir = c;
+    c->esq = Coringa;
+    c->peso = max(getPeso(c->esq), getPeso(c->dir))+1;
+    x->peso = max(getPeso(x->esq), getPeso(x->dir))+1;
+ 
+    return x;
+}
+
+C* giraEsquerda(struct C *c){
+    struct C *y = c->dir;
+    struct C *Coringa = y->esq;
+    y->esq = c;
+    c->dir = Coringa;
+    c->peso = max(getPeso(c->esq), getPeso(c->dir))+1;
+    y->peso = max(getPeso(y->esq), getPeso(y->dir))+1;
+ 
+    return y;
+}
+
+int found =0;
+C* rC(struct C* c,char n[10]){
+    if (c == NULL){
+        return c;
+    }
+    if (strcmp(n, c->n) < 0 ){
+        c->esq = rC(c->esq, n);
+    }else if (strcmp(n, c->n) > 0 ){
+        c->dir = rC(c->dir, n);
+    }else if (strcmp(n, c->n) == 0 ){
+        found =1;
+        if( (c->esq == NULL) || (c->dir == NULL) )
+        {
+            struct C *aux = c->esq ? c->esq : c->dir;
+            if (aux == NULL)
+            {
+                aux = c;
+                c = NULL;
+            }else{  
+                *c = *aux; 
+            }
+            free(aux);
+        }
+        else
+        {
+            C* aux = minValorNo(c->dir);
+            strcpy(c->n,aux->n);
+            c->dir = rC(c->dir, aux->n);
+        }
+    }
+    if (c == NULL){
+       return c;
+    }
+    
+    
+    c->peso = 1 + max(getPeso(c->esq),getPeso(c->dir));
+ 
+    int balance = 0;
+    if (c != NULL){
+       balance= getPeso(c->esq) - getPeso(c->dir);
+    }
+ 
+    if (balance > 1 && diferencapeso(c->esq) >= 0){
+        return giraDireita(c);
+    }
+
+    if (balance > 1 && diferencapeso(c->esq) < 0){
+        c->esq =  giraEsquerda(c->esq);
+        return giraDireita(c);
+    }
+
+    if (balance < -1 && diferencapeso(c->dir) <= 0){
+        return giraEsquerda(c);
+    }
+    if (balance < -1 && diferencapeso(c->dir) > 0){
+        c->dir = giraDireita(c->dir);
+        return giraEsquerda(c);
+    }
+ 
+    return c;
+}
+
+C* iC(C* t, int nu,char n[10]){
+    if(t == NULL)
     {
-        *t = (struct C *) malloc(sizeof(C));
-        (*t)->esq = NULL; 
-        (*t)->dir = NULL; 
-        (*t)-> nu= nu;
-        strcpy((*t)->n,n); 
-        //printf("%s\n",(*t)->n);
-        (*t)->dad = dad; 
-        return; 
+        C* c = (struct C *) malloc(sizeof(C));
+        c->esq = NULL; 
+        c->dir = NULL; 
+        c-> nu= nu;
+        strcpy(c->n,n); 
+        c-> peso= 1;
+        return c; 
     }
     
-    if(strcmp(n, (*t)->n) <0 ){
-        iC(&(*t)->esq, nu,n,*t);
-    }
 
-    if (strcmp(n, (*t)->n) == 0){
+    if(strcmp(n, t->n) < 0 ){
+        t->esq=iC(t->esq, nu,n);
+    }else if(strcmp(n, t->n) == 0){
         printf("Contatinho ja inserido\n");
-        return;
+        return t;
+    }else{
+        t->dir=iC(t->dir, nu,n);
     }
 
-    if(strcmp(n, (*t)->n) >0){
-        iC(&(*t)->dir, nu,n,*t);
+    t->peso = 1 + max(getPeso(t->esq),getPeso(t->dir));
+
+    int balance = 0;
+    if (t != NULL){
+       balance= getPeso(t->esq) - getPeso(t->dir);
     }
-    
+   
+    if (balance > 1 && strcmp(n,t->esq->n) < 0 ){
+        return giraDireita(t);
+    }
+   
+    if (balance < -1 && strcmp(n,t->dir->n) >0 ){
+        return giraEsquerda(t);
+    }
+
+    if (balance > 1 && strcmp(n,t->esq->n) >0 ) 
+    {
+        t->esq =  giraEsquerda(t->esq);
+        return giraDireita(t);
+    }
+ 
+  
+    if (balance < -1 &&  strcmp(n,t->dir->n) >0 ){
+        t->dir = giraDireita(t->dir);
+        return giraEsquerda(t);
+    }
+    return t;
 }
-C* cB()
-{
-    return NULL;
-}
+
 
 C* cF(C* c, char n[10]){
-    if (c!=NULL){
-        if (strcmp(n, c->n) == 0){
-            return c;
+    if (c == NULL){
+        return c;
+    }
+    struct C* atual = c;
+    for (;atual!=NULL;){
+        if(strcmp(n, atual->n) == 0){
+            break;
+        }else if(strcmp(n, atual->n) < 0){
+            atual = atual -> esq;
+        }else{
+            atual = atual -> dir;
         }
-        cF(c->dir,n);           
-        cF(c->esq,n);
     }
-}
-
-
-
-void cD(struct C* c){
-    if (c!=NULL){
-        cD(c->dir);
-        printf("%s %d\n",c->n,c->nu);
-	    cD(c->esq);
-    }
+    return atual; 
 }
 
 int main(){
-    C* c = cB();
+    struct C *c = NULL;
     char co;
    
     C* cf=NULL;
-    C* dad=NULL;
-    C* son=NULL;
     for (;;){
         int nu;
         char na[10];
-        scanf("%c",&co);
+        scanf("%c ",&co);
         switch (co)
         {
         case 'I':
-            scanf("%s %d",na,&nu);
-            iC(&c,nu,na,NULL);
+            scanf(" %s %d",na,&nu);
+            c = iC(c,nu,na);
             break;
         case 'P':
-            scanf("%s",na);
+            scanf(" %s",na);
             cf=cF(c,na);
             if (cf==NULL){
                 printf("Contatinho nao encontrado\n");
@@ -93,67 +229,33 @@ int main(){
             }
             printf("Contatinho encontrado: telefone %d\n",cf->nu);
             break;
+       
         case 'R':
-            scanf("%s",na);
-            cf=cF(c,na);
-            if (cf==NULL){
-                printf("Operacao invalida:contatinho nao encontrado\n");
+            scanf(" %s",na);
+            c=rC(c,na);
+            if (found == 0 ){
+                printf("Operacao invalida: contatinho nao encontrado\n");
                 continue;
             }
-            if ((cf->esq==NULL)&&(cf->dir==NULL)){
-                continue;
-            }
-            if (cf->esq==NULL){
-                dad= cf->dad;
-                if (dad->dir==cf){
-                    son=cf->dir;
-                    son->dad=dad;
-                    dad->dir = cf->dir;
-                }
-                if (dad->esq==cf){
-                    son=cf->esq;
-                    son->dad=dad;
-                    dad->esq = cf->dir;
-                }
-                free(cf);
-                continue;
-            }
-            if (cf->dir==NULL){
-                C* dad=NULL;
-                dad= cf->dad;
-                C* son=NULL;
-                if (dad->dir==cf){
-                    son=cf->dir;
-                    son->dad=dad;
-                    dad->dir = cf->dir;
-                }
-                if (dad->esq==cf){
-                    son=cf->esq;
-                    son->dad=dad;
-                    dad->esq = cf->dir;
-                }
-                free(cf);
-                continue;
-            }
+            found =0;
             break;
         case 'A':
-            scanf("%s",na);
+            scanf(" %s %d",na,&nu);
             cf=cF(c,na);
             if (cf==NULL){
-                printf("Operacao invalida:contatinho nao encontrado\n");
+                printf("Operacao invalida: contatinho nao encontrado\n");
                 continue;
             }
             cf->nu=nu;
             break;
-        case 'D':
-            cD(c);
-            break;
-        //case '0':
-            //free(c);
-            //return 0;
+
+        case '0':
+            free(c);
+            return 0;
         }
     }
    
 
 	return 0;
 } 
+
